dc19c1fcce5e7eac6462a04c3abfe80287352d38
unknown <255686@edu.p.lodz.pl>
2026-04-08 13:26:59 +0200

    Dodanie dokumentacji do klasy DefaultCountingOutRhymes

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,31 +4,52 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
+    /**
+     *
+     * @return
+     */
     public int getTotal() {
         return total;
     }
 
     private int total = -1;
 
+    /**
+     *
+     * @param in
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean callCheck() {
         return total == -1;
     }
-
+    /**
+     *
+     * @return
+     */
     public boolean isFull() {
         return total == 11;
     }
-
+    /**
+     *
+     * @return
+     */
     protected int peekaboo() {
         if (callCheck())
             return -1;
         return numbers[total];
     }
-
+    /**
+     *
+     * @return
+     */
     public int countOut() {
         if (callCheck())
             return -1;
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,8 +3,8 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
-class RhymersDemo {
 
+class RhymersDemo {
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
 
